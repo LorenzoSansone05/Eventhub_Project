d4c3543d7c26342a9cdaf5de1397cb55c1f754ac
Lorenzo Sansone <sansone.lorenzo05@gmail.com>
2026-05-25 17:49:04 +0200

    fix: validazione sul dto BookTicketRequestDTO

--- eventhub/src/main/java/it/academy/largesystems/eventhub/dto/BookTicketRequestDTO.java
+++ eventhub/src/main/java/it/academy/largesystems/eventhub/dto/BookTicketRequestDTO.java
@@ -1,6 +1,7 @@
 package it.academy.largesystems.eventhub.dto;
 
 import it.academy.largesystems.eventhub.entity.enums.TicketType;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookTicketRequestDTO {
+
+    @NotNull(message = "L'ID dell'utente è obbligatorio per effettuare una prenotazione.")
     private Long userId;
+
+    @NotNull(message = "La tipologia di biglietto (STANDARD o VIP) è obbligatoria.")
     private TicketType type;
 }
